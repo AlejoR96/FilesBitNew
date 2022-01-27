f409a121d08d988c83778f81c2c0b000b6ef60dc
Alejandro Rivera <jarivera636@misena.edu.co>
2022-01-26 23:13:15 -0500

    use code with template literals

---
+++ class15/template literals/index.js
@@ -0,0 +1,6 @@
+let string = 'estoy creada con comillas sencillas';
+let name = "Alejandro";
+let lastname = "Rivera";
+let stringWithBackticks = `Hola soy ${ name } ${ lastname }`;
+console.log(stringWithBackticks);
+
