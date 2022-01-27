e2456e0866af30803b4a3c39942f71696acc0565
Alejandro Rivera <jarivera636@misena.edu.co>
2022-01-26 23:06:28 -0500

    learning class if ternary's

---
+++ class15/if ternario/index.js
@@ -0,0 +1,13 @@
+if (true) {
+  console.log("is True");
+}else{
+  console.log("is False");
+}
+
+// if ternary
+
+let number = 6;
+
+number >= 5 
+? console.log("is True")
+: console.log("is False");
