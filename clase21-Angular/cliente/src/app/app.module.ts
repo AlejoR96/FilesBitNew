2430878709e2596d14b93b123bd9abd651c17c45
Alejandro Rivera <jarivera636@misena.edu.co>
2022-02-21 02:08:17 -0500

    Se diseño y se agregaron otras configuraciones al proyecto

--- clase21-Angular/cliente/src/app/app.module.ts
+++ clase21-Angular/cliente/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Pagina404Component } from './components/pagina404/pagina404.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pagina404Component,
+    LoginComponent,
+    HomeComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
