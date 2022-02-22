0b69b53c5f1970a009770167828f235a64171e80
Alejandro Rivera <jarivera636@misena.edu.co>
2022-02-22 02:29:09 -0500

    Introduccion a tsc - video35

---
+++ clase22/code.ts
@@ -0,0 +1,17 @@
+console.log("hola");
+
+var varTexto:string = "Texto";
+varTexto = '2';
+
+let letNumero:number =23;
+letNumero = 23;
+
+document.write(letNumero.toString());
+
+let letBooleanos: boolean = true || false;
+
+let letDinamica:any = " texto";
+letDinamica=90;
+letDinamica= false;
+letDinamica= ["Texto1", 14, true];
+letDinamica= {key1: "hola"};
