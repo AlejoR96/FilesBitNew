918255b0b6c245f07b293027645ae4886ca03f4d
Alejandro Rivera <jarivera636@misena.edu.co>
2022-02-23 00:34:07 -0500

    introduccion del localstorage y sessionstorage

---
+++ clase22/ejemplo.js
@@ -0,0 +1,25 @@
+// function prueba (variable = 2 ){
+//   return `El parametroes  ${variable}`;
+// }
+
+// console.log(prueba(55));
+
+// let nombre = "Alejo"
+// if (nombre === "Alejo") {
+//   console.log("Se llama Alejo");
+// }else{
+//   console.error("no se llama Alejo")
+// }
+
+// nombre === "Alejo" 
+// ? console.log("se llama Alejo")
+// : console.log("No se llama Alejo")
+
+let nombre = "Valeria";
+let apellido = "Perez";
+let edad = 84;
+
+
+sessionStorage.setItem("nombre_estudiante", nombre);
+sessionStorage.setItem("apellido_estudiante", apellido);
+sessionStorage.setItem("edad_estudiante", edad);
