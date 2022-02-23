5a94f8a084270f7f4926a59600cc05e4ca9a5c20
Alejandro Rivera <jarivera636@misena.edu.co>
2022-02-23 01:47:33 -0500

    Metodo para guardar y eliminar info en localstorage

--- clase22/code.js
+++ clase22/code.js
@@ -50,3 +50,35 @@ function saludo(nombre, apellido) {
     return "Hola ".concat(nombre, " ").concat(apellido, ", bienvenido....");
 }
 console.log(saludo('Alejo', 'Rivera'));
+/* Reto - TSC */
+function realizarProceso(valor, accion_usuario) {
+    var localArray = localStorage.getItem("nuestro_array");
+    switch (accion_usuario) {
+        case 'agregar':
+            if (localStorage.getItem("nuestro_array") === null) {
+                localStorage.setItem("nuestro_array", valor);
+            }
+            else {
+                console.log(localArray);
+                localArray = localArray.split(',');
+                console.log(localArray);
+                localArray.push(valor);
+                localStorage.setItem("nuestro_array", localArray);
+                break;
+            }
+        case 'eliminar':
+            localArray = localStorage.getItem("nuestro_array");
+            localArray = localArray.split(',');
+            if (localArray.length > 1) {
+                localArray.pop();
+                localStorage.setItem("nuestro_array", localArray);
+            }
+            else {
+                localStorage.removeItem("nuestro_array");
+            }
+            break;
+        default:
+            break;
+    }
+}
+realizarProceso('miValor', 'eliminar');
