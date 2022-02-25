465f5c1753dfc193fcacf329365499dadccaadf4
Alejandro Rivera <jarivera636@misena.edu.co>
2022-02-25 03:29:39 -0500

    Se configuro el constructor con el fb y sus respectivo variables para el form reactivo

--- clase21-Angular/cliente/src/app/components/login/login.component.ts
+++ clase21-Angular/cliente/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// Importacion de modulo para realizar grupos de formularios
+
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    // variables de tipo formgroup
+
+    loginForm: FormGroup;
+    validacionEmail= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/;
+  constructor(private fb: FormBuilder) {
+      this.loginForm = this.fb.group({
+        // usuario:['', [Validators.required, Validators.email]],
+        usuario:['', [Validators.required, Validators.pattern(this.validacionEmail)]],
+
+        password: ['', Validators.required]
+      });
+  }
 
   ngOnInit(): void {
   }
-
+ accesoUsuario(){
+     console.log(this.loginForm);
+ }
 }
